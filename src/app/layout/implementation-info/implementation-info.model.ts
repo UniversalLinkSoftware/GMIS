70d669c689c85eb90ab6aa1f133d2bb3d2f4489e
uzzwalbadal <uzzwal19@gmail.com>
2019-01-27 15:21:39 +0545

    models for social information and implementation information was added with their proper validation

---
+++ src/app/layout/implementation-info/implementation-info.model.ts
@@ -0,0 +1,16 @@
+export class Implementation {
+    constructor(public  gonShare: number,
+         public wuaShare: number,
+         public physicalProgress:string,
+         public physicalProgressDate:Date,
+         public finiancialProgress:string,
+         public finiancialProgressDate:Date,
+         public wuaDevelopmentOffice:string,
+         public wuaRegistrarionDate:Date,
+         public trainingName:string,
+         public trainingDate:Date,
+         public trainingPeriod:number,
+         public numberOfParticipants:number,
+         
+         ) {}
+}
