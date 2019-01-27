d7aa47a8539f9ac2c179824ae478d84fa99d8598
uzzwalbadal <uzzwal19@gmail.com>
2019-01-27 15:47:53 +0545

    economic model and location model was added !!

---
+++ src/app/layout/economic-info/economic-info.model.ts
@@ -0,0 +1,10 @@
+export class Economic {
+    constructor(public  totalProjectCost:number,
+        public bcRatio1:number,
+        public costingYear: Date,
+        public discountRate1:number,
+        public eirr:number,
+        public bcRatio2:number,
+        public discountRatio2:number,
+         ) {}
+}
