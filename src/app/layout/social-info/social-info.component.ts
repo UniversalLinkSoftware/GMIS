df7870849e777596c55d13374a928135b9e21cd6
prahyas <xrestha17@gmail.com>
2019-02-17 15:47:08 +0545

    Added dynamic table in Social info and UI uodated.

--- src/app/layout/social-info/social-info.component.ts
+++ src/app/layout/social-info/social-info.component.ts
@@ -1,7 +1,7 @@
 import { SocialService } from './social.service';
 import { Social } from './social-info.model';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { DataStorageService } from './data-storage.service';
 import { Response } from '@angular/http';
 import { Subscription } from 'rxjs';
@@ -12,6 +12,7 @@ import { Subscription } from 'rxjs';
 })
 export class SocialInfoComponent implements OnInit {
   page3Form: FormGroup;
+  TotalRow: number;
  // socialInfo: Social;
   subscription: Subscription;
    aipf = 0;
@@ -29,7 +30,7 @@ export class SocialInfoComponent implements OnInit {
    wihh = 0;
 
 
-  constructor(private socialService: SocialService, private dataStorageService: DataStorageService ) {}
+  constructor(private socialService: SocialService, private dataStorageService: DataStorageService, private _fb: FormBuilder ) {}
   onSubmit() {
       this.socialService.addSocial(this.page3Form.value);
     }
@@ -55,6 +56,7 @@ ngOnInit() {
 
         this.page3Form =
   new FormGroup({
+    tableRows: this._fb.array([this.initTableRows()]),
     'numberOfHousehold': new FormControl(this.numhh, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
     'male': new FormControl(this.mle, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
     'female': new FormControl(this.fml, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
@@ -73,12 +75,30 @@ ngOnInit() {
   });
 
 
-
-
-
-
-
+}
+initTableRows() {
+  return this._fb.group({
+    Select: [''],
+    Number: [''],
+    Percentage: [''],
+  });
 }
 
+addNewRows() {
+  const control = <FormArray>this.page3Form.controls['tableRows'];
+  control.push(this.initTableRows());
+}
 
+deleteRows(index: number) {
+const control = <FormArray>this.page3Form.controls['tableRows'];
+if (control != null) {
+  this.TotalRow = control.value.length;
+}
+if (this.TotalRow > 1) {
+  control.removeAt(index);
+} else {
+  alert('one record is mandatory.');
+  return false;
+}
+}
 }
