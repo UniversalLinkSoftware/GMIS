6e48287dcdec7c8c726f648c242276f6c4423e7a
uzzwalbadal <uzzwal19@gmail.com>
2019-03-14 12:36:37 +0545

    print to pdf function added to social,implementation and groundwaterinfo!!

--- src/app/layout/groundwater-info/groundwater-view/groundwater-view.component.ts
+++ src/app/layout/groundwater-info/groundwater-view/groundwater-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Groundwater } from '../groundwater-info.model';
 import { GroundwaterService } from '../groundwater.service';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-groundwater-view',
@@ -8,11 +10,28 @@ import { GroundwaterService } from '../groundwater.service';
   styleUrls: ['./groundwater-view.component.scss']
 })
 export class GroundwaterViewComponent implements OnInit {
+  groundwater: Array<Groundwater>;
+  public captureScreen() {
+    const data = document.getElementById('a');
+    html2canvas(data).then(canvas => {
+
+      const imgWidth = 210;
+
+      const imgHeight = 297;
+      const heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4');
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('implementation.pdf');
+    });
+  }
 
   constructor(private groundwaterService: GroundwaterService) {
 
    }
-   groundwater: Array<Groundwater>;
+
     ngOnInit() {
     this.groundwaterService.getAll().subscribe(data => {
       this.groundwater = data;
